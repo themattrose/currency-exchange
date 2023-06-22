c02b5c4c92d4462e9a6b26de19caf013970a2aab
Matt Rose <mattroseinbox@gmail.com>
2023-06-23 00:15:02 +0200

    feat: implement common UI

---
+++ src/components/UI/index.ts
@@ -0,0 +1,25 @@
+import { COLORS } from "@/constants/colors";
+import styled from "@emotion/styled";
+
+export const PageTitle = styled.h1`
+  font-size: 3rem;
+  font-weight: bold;
+  margin: 2rem 0;
+  color: ${COLORS.TEXT_DEFAULT};
+`;
+
+export const SectionTitle = styled.h3`
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin: 1rem 0;
+  color: ${COLORS.TEXT_DEFAULT};
+`;
+
+export const FlexRow = styled.div`
+  display: flex;
+`;
+
+export const FlexColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
